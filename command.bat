1bc1757be791f51894841c9ce4922751b2166b6b
Shixiong.Liu <liushixiong@corp.netease.com>
2022-12-01 11:28:34 +0800

    chore:更新command.bat

--- command.bat
+++ command.bat
@@ -4,8 +4,9 @@ cd qtcreator_build
 SET PATH=D:\soft_dev\build_qt_devs\cmake-3.22.0\bin;%PATH%
 SET PATH=D:\soft_dev\build_qt_devs\ninja-win;%PATH%
 SET PATH=C:\python3.10.6_amd64;%PATH%
+SET LLVM_INSTALL_DIR=D:\soft_dev\build_qt_devs\LLVM-15.0.2-win64
 
-cmake -DCMAKE_BUILD_TYPE=RelWithDebInfo -G Ninja "-DCMAKE_PREFIX_PATH=D:\qt\qt_5.15.2_x64\bin;D:\soft_dev\build_qt_devs\LLVM-15.0.2-win64" F:\qt_creator\qt-creator-opensource-src-8.0.0-rc1
+cmake -DCMAKE_BUILD_TYPE=RelWithDebInfo -G Ninja "-DCMAKE_PREFIX_PATH=D:\qt\qt_5.15.2_x64\bin" F:\qt_creator\qt-creator-opensource-src-8.0.0-rc1
 
 REM 编译
 REM cmake --build . 
